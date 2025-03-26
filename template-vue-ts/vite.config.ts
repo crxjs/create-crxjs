fa8969ff3ec7400a2e0d6acd849cde6b9e7a9751
FliPPeDround <734243792@qq.com>
2025-03-26 11:03:56 +0800

    feat: template-vue

---
+++ template-vue-ts/vite.config.ts
@@ -0,0 +1,21 @@
+import { crx } from '@crxjs/vite-plugin'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import manifest from './manifest.config.ts'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    crx({ manifest }),
+  ],
+  legacy: {
+    skipWebSocketTokenCheck: true,
+  },
+  server: {
+    cors: {
+      origin: [
+        /chrome-extension:\/\//,
+      ],
+    },
+  },
+})
