2dec983f60962f744d330bbb2b2867a50eaf226d
FliPPeDround <734243792@qq.com>
2025-03-26 14:56:01 +0800

    feat: template-react

---
+++ template-react-ts/vite.config.ts
@@ -0,0 +1,21 @@
+import { crx } from '@crxjs/vite-plugin'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import manifest from './manifest.config.js'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    crx({ manifest }),
+  ],
+  legacy: {
+    skipWebSocketTokenCheck: true,
+  },
+  server: {
+    cors: {
+      origin: [
+        /chrome-extension:\/\//,
+      ],
+    },
+  },
+})
