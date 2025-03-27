45da4cefd4c42ba9d7c1b9abfab6fabf3ec6e812
flippedround <734243792@qq.com>
2025-03-27 21:53:27 +0800

    feat: add template-solid content script UI

---
+++ template-solid-ts/vite.config.ts
@@ -0,0 +1,27 @@
+import path from 'node:path'
+import { crx } from '@crxjs/vite-plugin'
+import { defineConfig } from 'vite'
+import solid from 'vite-plugin-solid'
+import manifest from './manifest.config.ts'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': `${path.resolve(__dirname, 'src')}`,
+    },
+  },
+  plugins: [
+    solid(),
+    crx({ manifest }),
+  ],
+  legacy: {
+    skipWebSocketTokenCheck: true,
+  },
+  server: {
+    cors: {
+      origin: [
+        /chrome-extension:\/\//,
+      ],
+    },
+  },
+})
