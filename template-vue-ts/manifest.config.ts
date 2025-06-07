735453eee35e8c2f8a19dd41623f4e1c24cab325
flippedround <734243792@qq.com>
2025-06-07 12:28:58 +0800

    feat(manifest): add icon declaration to all template projects

--- template-vue-ts/manifest.config.ts
+++ template-vue-ts/manifest.config.ts
@@ -5,6 +5,9 @@ export default defineManifest({
   manifest_version: 3,
   name: pkg.name,
   version: pkg.version,
+  icons: {
+    48: 'public/logo.png',
+  },
   action: {
     default_icon: 'public/logo.png',
     default_popup: 'src/popup/index.html',
